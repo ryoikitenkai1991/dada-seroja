e4235685aedcfe2e767dfafc124ad090070b8712
LibrarianLibrarian <prod.libararian@gmail.com>
2024-03-14 10:52:49 +0200

    muriyokusho

---
+++ script.js
@@ -0,0 +1,104 @@
+// const un = `dyadya seroja`
+// const pas = `gkip2554`
+
+// const enterpas = prompt(`kakoy u tebya paroll?`)
+// const nik = prompt(`vedite svoy nik`)
+// if(pas === enterpas && un === nik){
+//     alert(`dobro pojalovat`)
+// }else{
+//     alert(`gulay otsuda neudachnik`)
+// }
+
+// let bill = 49;
+// if(bill > 0 && bill < 55 ){
+//     alert(`schet ${bill}, obsluga ${bill * 0.1}, obshiy scot ${bill + bill * 0.1 }`);
+// }else if( bill > 50 && bill < 105){
+//   alert(`schet ${bill}, obsluga ${bill * 0.15}, obshiy scot ${bill + bill * 0.15}`)
+// }
+
+// else{
+//     alert(`schet ${bill}, obsluga ${bill * 0.2}, obshiy scot ${bill + bill * 0.2}`)
+// }
+
+
+
+
+
+
+
+
+
+
+
+// let js = "";
+
+// let element = document.createElement("h1");
+// element.textContent = js;
+
+// let div1 = document.querySelector(".d1");
+// div1.append(element)
+
+
+
+
+
+
+
+
+// let littel = "Abbos Valiev";
+
+// let h1 = document.createElement ("h1");
+// h1.textContent = littel;
+
+// let div = document.querySelector (".d1");
+// div.append (h1);
+// let img2 = document.createElement("img");
+// img.src = "https://img.lovepik.com/png/20231116/angry-man-clipart-cartoon-of-angry-man-character-vector-characters_607027_wh860.png"
+
+// let div2 = document.querySelector(".d2");
+// div2.append(img)
+
+
+
+// let num = `jj`;
+// let age = 13;
+// alert(`privet mena zavut ${num}, moy vozrast ${age} let`);
+
+
+// const klas = prompt(`kak tebya zovut?`)
+// let year = prompt(`skolko tebe let?`)
+// const line = prompt(`v kakom godu ti rodilsa?`)
+// alert(`menya zovut ${klas}, mne ${year} let, ya rodilsa v ${line} godu `)
+
+
+// alert(123456789*123456789)
+
+
+
+// const num = '16';
+// const line = prompt(`napishi skolko tebe let i mi uznaem mojno tebe vodit mashinu ili net`);
+// alert(num < line);
+
+
+// let num= 5;
+
+// let num2= 5;
+// let num3 = 5;
+
+// num += 1;
+
+// num2 -= 1;
+// num3 += 5;
+
+// let total = num + num2 + num3; 
+// alert(total)
+
+
+
+// if(age > 18){
+//     alert(``)
+
+// }else{
+
+// }
+
